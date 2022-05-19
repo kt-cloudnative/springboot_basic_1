49f3e250a875de523a80c7f1074bf04635f39417
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-05-19 14:50:18 +0900

    Update Article.java

--- src/main/java/com/kt/edu/firstproject/entity/Article.java
+++ src/main/java/com/kt/edu/firstproject/entity/Article.java
@@ -1,6 +1,9 @@
 package com.kt.edu.firstproject.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
