fbfb95c2219d873eb009a6d29b4ca2c1c81ddd53
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2022-05-19 14:27:38 +0900

    Update Article.java

--- src/main/java/com/kt/edu/firstproject/entity/Article.java
+++ src/main/java/com/kt/edu/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.kt.edu.firstproject.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
