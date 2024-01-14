480f55b812297099aa34a0375c8302d6e900ea70
shclub <shclub@gmail.com>
2024-01-14 13:09:47 +0900

    first commit

--- src/main/java/com/kt/edu/firstproject/entity/Article.java
+++ src/main/java/com/kt/edu/firstproject/entity/Article.java
@@ -1,7 +1,6 @@
 package com.kt.edu.firstproject.entity;
 
-import javax.persistence.*;
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
